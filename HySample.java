7687f9d6b9276a8662b4c04ae5abb3abcd7f91f6
Poonam.T <poonamtuptewar@gmail.com>
2026-07-01 22:03:45 +0530

    HySample.java

---
+++ HySample.java
@@ -0,0 +1,13 @@
+package com.ssit.HySample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HySampleApplication.class, args);
+	}
+
+}
