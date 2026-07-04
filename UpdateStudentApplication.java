ea10dd475b396fa5560a77eac80090def7ab3af1
Poonam.T <poonamtuptewar@gmail.com>
2026-07-04 20:04:03 +0530

    UpdateStudentApplication.java

---
+++ UpdateStudentApplication.java
@@ -0,0 +1,56 @@
+package com.example.Student;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication implements CommandLineRunner {
+
+	@Autowired
+	private StudRepository repository;
+
+	public StudentApplication(StudRepository repository) {
+		this.repository = repository;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Student s1 = new Student(101, "Poonam", 90, "IT");
+		Student s2 = new Student(102, "Akarshita", 90, "IT");
+		Student s3 = new Student(103, "Athiya", 90, "IT");
+
+		repository.save(s1);
+		repository.save(s2);
+		repository.save(s3);
+
+		System.out.println("Student saved successfully...");
+
+		System.out.println("Updating Student...");
+
+		Optional<Student> temp = repository.findById(101);
+
+		if (temp.isPresent()) {
+
+			Student student = temp.get();
+
+			student.setName("Poonam Tuptewar");
+			student.setPer(95);
+			student.setBranch("Computer");
+
+			repository.save(student);
+
+			System.out.println("Student Updated Successfully...");
+			System.out.println(student);
+
+		}
+	}
+
+}
